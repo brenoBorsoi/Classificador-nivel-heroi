205dd59ea5bf7ade047264e81f1401f1a2f84c41
Breno Borsoi <brenoborsoi@gmail.com>
2024-10-24 20:49:32 -0300

    entrega do projeto

--- index.js
+++ index.js
@@ -1,48 +1,40 @@
 const prompt = require("prompt-sync")({sigint:true});
 
-let nomeHeroi = prompt("Digite o nome do herói: ");
-let nivelHeroi = 1
-let xpHeroi = 1
-let xpGanho = 333
+let Victories = prompt("Digite quantas Vitórias seu herói teve: ");
+let Defeats = prompt("Digite quantas defeat seu herói teve: ");
 
-let escolha = prompt("Digite 1 para adicionar xp manualmente ou 2 para ganhar xp derrotando inimigos: ")
+let winningBalance
 
-if (escolha == 1) {
+balanceCalculation(Victories, Defeats)
 
-    xpHeroi = prompt("Digite a xp do Herói: ")
-
-} else {
-    
-    let i = prompt("digite quantos inimigos seu herói derrotou (quantas vezes será rodado o laço de repetição: ");
-
-do { 
-    
-    console.log("Voce derrotou " + 1 + " inimigo. e ganhou " + xpGanho + " de XP")
-    xpHeroi += xpGanho
-    i--
-} while (i >= 0);
-
+function balanceCalculation(Victories, Defeats) {
+    winningBalance = Victories - Defeats
+   // console.log("o saldo é: " + winningBalance);
 }
 
+levelCalculation()
 
-console.log("A XP do " + nomeHeroi + " é de: " + xpHeroi);
+function levelCalculation() {
 
+    let result
 
-if (xpHeroi <= 1000) {
-    console.log("O herói de nome " + nomeHeroi + " está no nível de Ferro");
-} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
-    console.log("O herói de nome " + nomeHeroi + " está no nível de Bronze");
-} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
-    console.log("O herói de nome " + nomeHeroi + " está no nível de Prata");
-} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
-    console.log("O herói de nome " + nomeHeroi + " está no nível de Ouro");
-} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
-    console.log("O herói de nome " + nomeHeroi + " está no nível de Platina");
-} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
-    console.log("O herói de nome " + nomeHeroi + " está no nível de Ascendente");
-} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
-    console.log("O herói de nome " + nomeHeroi + " está no nível de Imortal");
-} else if (xpHeroi >= 10001) {
-    console.log("O herói de nome " + nomeHeroi + " está no nível de Radiante");
-     
+    if (winningBalance <= 10) {
+        result = "Ferro";
+    } else if (winningBalance >= 11 && winningBalance <= 20) {
+        result = "Bronze";
+    } else if (winningBalance >= 21 && winningBalance <= 50) {
+        result = "Prata";
+    } else if (winningBalance >= 51 && winningBalance <= 80) {
+        result = "Ouro";
+    } else if (winningBalance >= 81 && winningBalance <= 90) {
+        result = "Diamante";
+    } else if (winningBalance >= 91 && winningBalance <= 100) {
+        result = "Lendário";
+    } else if (winningBalance >= 101) {
+        result = "Imortal";
+    }
+
+    return console.log("O herói tem de saldo de " + winningBalance + " está no nível de " + result);
+  
 }
+
